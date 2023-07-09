66e4e5155aa99e08f714ce1232c2af66a2f1a4c3
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-09 15:06:54 -0300

    Adicionando codigo que verifica a baixa do estoque

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -4,10 +4,9 @@ const getLowStockProducts = () => {
   let produtLittle = [];
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (stockProducts[index].quantityInStock === 0 || stockProducts[index].quantityInStock <= 10) {
-      produtLittle.push(stockProducts[index].productName);
+      produtLittle.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`);
     }
   }
-  console.log(produtLittle.length);
   return produtLittle;
 };
 console.log(getLowStockProducts());
