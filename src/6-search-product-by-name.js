633cdee5351eca3a5270dbae47d7633cc94ca56c
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-09 15:29:02 -0300

    Criando codigo localiza produto

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,11 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (nameProduct) => {
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (nameProduct === stockProducts[index].productName) {
+      console.log(`${stockProducts[index].description} R$${stockProducts[index].price}`);
+    }
+  }
 };
-
+console.log(searchProductByName());
 module.exports = { searchProductByName };
