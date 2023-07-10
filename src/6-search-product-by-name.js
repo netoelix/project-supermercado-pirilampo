cbb71d40345f15de53093af0fd446185ae851ecd
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-10 15:34:42 -0300

    Testando return no codigo

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -3,9 +3,9 @@ const stockProducts = require('./data.json');
 const searchProductByName = (nameProduct) => {
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (nameProduct === stockProducts[index].productName) {
-      console.log(`${stockProducts[index].description} R$${stockProducts[index].price}`);
+      return `${stockProducts[index].description} R$${stockProducts[index].price}`;
     }
   }
 };
-console.log(searchProductByName());
+console.log(searchProductByName('Arroz'));
 module.exports = { searchProductByName };
