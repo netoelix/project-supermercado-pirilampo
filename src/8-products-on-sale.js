5c57bc534806fa5fb5a2a73eddeff6118b437ca6
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 19:39:29 -0300

    Tirando codigo do array vazio

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -2,7 +2,6 @@ const stockProducts = require('./data.json');
 
 const getProductsOnSale = (nameSale) => {
   let onSale = [];
-  let onSaleStatus = false;
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (nameSale === stockProducts[index].productName) {
       let onSales = {
@@ -11,13 +10,9 @@ const getProductsOnSale = (nameSale) => {
         onSale: stockProducts[index].onSale,
       };
       onSale.push(onSales);
-      onSaleStatus = true;
     }
-  }
-  if (!onSaleStatus) {
-    onSale = [];
   }
   return onSale;
 };
-console.log(getProductsOnSale('Arroz'));
+console.log(getProductsOnSale());
 module.exports = { getProductsOnSale };
