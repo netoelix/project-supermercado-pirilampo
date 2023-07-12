00ddfdad1e91b0fe6729d4501e150df272a9e700
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 20:45:02 -0300

    testando o metodo join para converter o array em string

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,13 +1,13 @@
 const stockProducts = require('./data.json');
 
-const getProductsWithAllergyOrIntoleranceInfo = () => {
+const getProductsWithAllergyOrIntoleranceInfo = (nameProduct) => {
   let searchProduct = [];
   for (let index = 0; index < stockProducts.length; index += 1) {
-    if (stockProducts[index].allergyOrIntolerance) {
+    if (nameProduct === stockProducts[index].productName) {
       let productsAllergyOrIntolerance = {
         description: stockProducts[index].description,
         formattedPrice: `R$ ${stockProducts[index].price}`,
-        allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance}`,
+        allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance.join(' ')}`,
       };
       searchProduct.push(productsAllergyOrIntolerance);
     }
