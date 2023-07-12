ba2ec7d490717d76f29c1fb157fac5c3ab2855bb
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 18:19:34 -0300

    Corrigindo logica do codigo

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,10 +1,15 @@
 const stockProducts = require('./data.json');
 
 const searchProductByName = (nameProduct) => {
+  let searchProduct = { description: '123', formattedPrice: '123' };
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (nameProduct === stockProducts[index].productName) {
-      return `${stockProducts[index].description} R$${stockProducts[index].price}`;
+      searchProduct.description = stockProducts[index].description;
+      searchProduct.formattedPrice = `R$${stockProducts[index].price}`;
+    } else {
+      searchProduct = null;
     }
+    return searchProduct;
   }
 };
 console.log(searchProductByName('Arroz'));
