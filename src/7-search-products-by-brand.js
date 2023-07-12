ce73183b4481b64155276f3394567ccab3d94d85
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 18:46:27 -0300

    Corrigindo logica do codigo procura pela marca

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,19 +1,22 @@
 const stockProducts = require('./data.json');
 
 const searchProductsByBrand = (nameBrand) => {
-  let searchBrand = { description: '123', formattedPrice: '123' };
+  let searchBrands = [];
   let foundBrand = false;
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (nameBrand === stockProducts[index].brand) {
-      searchBrand.description = stockProducts[index].description;
-      searchBrand.formattedPrice = `R$${stockProducts[index].price}`;
+      let searchBrand = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$${stockProducts[index].price}`
+      }
+      searchBrands.push(searchBrand);
       foundBrand = true;
     }
   }
   if (!foundBrand) {
-    searchBrand = null;
+    searchBrands = null;
   }
-  return searchBrand;
+  return searchBrands;
 };
 console.log(searchProductsByBrand('Hortifruti'));
 module.exports = { searchProductsByBrand };
