c856e3e52b0dc9eb70a3e70510639f0e0bc34d94
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 20:48:56 -0300

    Criando codigo, procura vitaminas

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,18 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productVitamin = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].nutritionalInfo.vitamins) {
+      let productVitamins = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        vitaminsInformation: stockProducts[index].nutritionalInfo.vitamins,
+      };
+      productVitamin.push(productVitamins);
+    }
+  }
+  return productVitamin;
 };
-
+console.log(getProductsRichInVitamin());
 module.exports = { getProductsRichInVitamin };
