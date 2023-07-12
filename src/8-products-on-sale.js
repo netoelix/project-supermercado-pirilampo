63bc810c5dbcb64ec37a8b3f8880e2ce29205708
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 19:08:07 -0300

    Criando codigo localiza produto on sale

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,23 @@
 const stockProducts = require('./data.json');
 
-const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+const getProductsOnSale = (nameSale) => {
+  let onSale = [];
+  let onSaleStatus = false;
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (nameSale === stockProducts[index].productName) {
+      let onSales = {
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        onSale: stockProducts[index].onSale,
+      };
+      onSale.push(onSales);
+      onSaleStatus = true;
+    }
+  }
+  if (!onSaleStatus) {
+    onSale = [];
+  }
+  return onSale;
 };
-
+console.log(getProductsOnSale('Arroz'));
 module.exports = { getProductsOnSale };
