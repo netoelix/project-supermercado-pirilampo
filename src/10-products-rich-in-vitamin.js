4db4b99d6a4fc6f1ff5f09fea36b12286edf2fc4
Elix Merlino Neto <neto.elix@outlook.com>
2023-07-12 21:36:58 -0300

    alterado codigo vitaminas

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,17 +2,23 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   let productVitamin = [];
+
   for (let index = 0; index < stockProducts.length; index += 1) {
     if (stockProducts[index].nutritionalInfo.vitamins) {
+      let vitaminsDisponible = [];
+      vitaminsDisponible.push(stockProducts[index].nutritionalInfo.vitamins);
+
       let productVitamins = {
         description: stockProducts[index].description,
         formattedPrice: `R$ ${stockProducts[index].price}`,
-        vitamins: stockProducts[index].nutritionalInfo.vitamins,
+        vitamins: vitaminsDisponible,
       };
       productVitamin.push(productVitamins);
     }
   }
+
   return productVitamin;
 };
+
 console.log(getProductsRichInVitamin());
 module.exports = { getProductsRichInVitamin };
